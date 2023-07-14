2bd197561683239f8ff12268fb66c4bdd6fc0bba
BilalAbouBaraki <bilalaboub1452001@gmail.com>
2023-07-14 18:15:49 +0300

    commit message la hayda elmessage

---
+++ script.js
@@ -0,0 +1,9 @@
+/*let output = document.getElementById('output');
+function selectOption() {
+   let dropdown = document.getElementById('dropdown');
+   // get the index of the selected option
+   let selectedIndex = dropdown.selectedIndex;
+   // get a selected option and text value using the text property
+   let selectedValue = dropdown.options[selectedIndex].text;
+   output.innerHTML = "The selected value is " + selectedValue;
+}*/
